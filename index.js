d9fd1b2e4842c8ff4ebf8cd051d48bf36ad2dcb0
myunggi_moon@naver.com <39259072+myunggiM@users.noreply.github.com>
2022-07-22 22:27:47 +0900

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a,i)=>{
+        a.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtP0slmouWiWPi_5Wc4ApCplxx-mR77lPSJw&usqp=CAU'
+    })
+
+},500)
