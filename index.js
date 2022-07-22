e0abaaafeff6f953b9813f0d3b475f7b93e04d32
myunggi_moon <myunggi_moon@naver.com>
2022-07-22 22:29:36 +0900

    make folder

--- index.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-
-    let imgs = document.querySelectorAll('img');
-    imgs.forEach((a,i)=>{
-        a.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtP0slmouWiWPi_5Wc4ApCplxx-mR77lPSJw&usqp=CAU'
-    })
-
-},500)
